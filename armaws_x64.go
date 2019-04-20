cb31398d6ea2229f8625cc72c9339e9fe24e9810
code34 <nicolas_boiteux@yahoo.fr>
2019-04-20 10:46:28 +0200

    delete useless code

--- armaws_x64.go
+++ armaws_x64.go
@@ -27,30 +27,8 @@ func RVExtensionVersion(output *C.char, outputsize C.size_t) {
 	C.memmove(unsafe.Pointer(output), unsafe.Pointer(result), size)
 }
 
-//export RVExtensionArgs
-func RVExtensionArgs(output *C.char, outputsize C.size_t, input *C.char, argv **C.char, argc C.int) {
-	var offset = unsafe.Sizeof(uintptr(0))
-	var out []string
-	for index := C.int(0); index < argc; index++ {
-		out = append(out, C.GoString(*argv))
-		argv = (**C.char)(unsafe.Pointer(uintptr(unsafe.Pointer(argv)) + offset))
-	}
-	temp := fmt.Sprintf("Function: %s nb params: %d params: %s!", C.GoString(input), argc,  out)
-
-	// Return a result to Arma
-	result := C.CString(temp)
-	defer C.free(unsafe.Pointer(result))
-	var size = C.strlen(result) + 1
-	if size > outputsize {
-		size = outputsize
-	}
-	C.memmove(unsafe.Pointer(output), unsafe.Pointer(result), size)
-}
-
 //export RVExtension
 func RVExtension(output *C.char, outputsize C.size_t, input *C.char) {
-	//parameters := fmt.Sprintf("%s", C.GoString(input))
-
 	parameters := strings.Split(fmt.Sprintf(C.GoString(input)), ";")
 	temp := callWS(parameters)
 
